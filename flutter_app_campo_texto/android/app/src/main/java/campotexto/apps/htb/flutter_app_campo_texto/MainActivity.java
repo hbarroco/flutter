c0767b341ca795bfb4815b1650ed60b06ef42c4c
Helio <hbarroco@ciandt.com>
2019-09-12 15:46:09 -0300

    Flutter - Campo Texto

---
+++ flutter_app_campo_texto/android/app/src/main/java/campotexto/apps/htb/flutter_app_campo_texto/MainActivity.java
@@ -0,0 +1,13 @@
+package campotexto.apps.htb.flutter_app_campo_texto;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ flutter_app_campo_texto/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_campo_texto/CampoTexto.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: CampoTexto(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
