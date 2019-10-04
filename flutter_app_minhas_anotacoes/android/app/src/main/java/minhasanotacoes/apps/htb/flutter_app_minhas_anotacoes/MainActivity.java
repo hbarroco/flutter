253d5e48372005d140c6b66f61dde9390e77e62c
Helio Barroco <heliobarroco@gmail.com>
2019-10-04 12:05:18 -0300

    Flutter App - Minhas Anotações - sqflite

---
+++ flutter_app_minhas_anotacoes/android/app/src/main/java/minhasanotacoes/apps/htb/flutter_app_minhas_anotacoes/MainActivity.java
@@ -0,0 +1,13 @@
+package minhasanotacoes.apps.htb.flutter_app_minhas_anotacoes;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
