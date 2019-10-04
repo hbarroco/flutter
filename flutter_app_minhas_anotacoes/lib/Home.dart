6562df384e191d0538c6035d374cb1cea7a5a949
Helio Barroco <heliobarroco@gmail.com>
2019-10-04 14:37:04 -0300

    Flutter Apps - Minhas Anotações

--- flutter_app_minhas_anotacoes/lib/Home.dart
+++ flutter_app_minhas_anotacoes/lib/Home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-
+import 'package:intl/date_symbol_data_local.dart';
+import 'helper/AnotacaoHelper.dart';
+import 'model/Anotacao.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,14 +13,27 @@ class _HomeState extends State<Home> {
 
   TextEditingController _tituloController = TextEditingController();
   TextEditingController _descricaoController = TextEditingController();
+  var _db = AnotacaoHelper();
+  List<Anotacao> _anotacoes = List<Anotacao>();
 
-  _exibirTelaCadastro(){
+  _exibirTelaCadastro( {Anotacao anotacao} ){
+
+    String textoSalvarAtualizar = "";
+    if( anotacao == null ){//salvando
+      _tituloController.text = "";
+      _descricaoController.text = "";
+      textoSalvarAtualizar = "Salvar";
+    }else{//atualizar
+      _tituloController.text = anotacao.titulo;
+      _descricaoController.text = anotacao.descricao;
+      textoSalvarAtualizar = "Atualizar";
+    }
 
     showDialog(
         context: context,
         builder: (context){
           return AlertDialog(
-            title: Text("Adicionar anotação"),
+            title: Text("$textoSalvarAtualizar anotação"),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -48,17 +63,91 @@ class _HomeState extends State<Home> {
                   onPressed: (){
 
                     //salvar
-
+                    _salvarAtualizarAnotacao(anotacaoSelecionada: anotacao);
 
                     Navigator.pop(context);
                   },
-                  child: Text("Salvar")
+                  child: Text(textoSalvarAtualizar)
               )
             ],
           );
         }
     );
 
+  }
+
+  _recuperarAnotacoes() async {
+
+    List anotacoesRecuperadas = await _db.recuperarAnotacoes();
+
+    List<Anotacao> listaTemporaria = List<Anotacao>();
+    for( var item in anotacoesRecuperadas ){
+
+      Anotacao anotacao = Anotacao.fromMap( item );
+      listaTemporaria.add( anotacao );
+
+    }
+
+    setState(() {
+      _anotacoes = listaTemporaria;
+    });
+    listaTemporaria = null;
+
+    //print("Lista anotacoes: " + anotacoesRecuperadas.toString() );
+
+  }
+
+  _salvarAtualizarAnotacao( {Anotacao anotacaoSelecionada} ) async {
+
+    String titulo = _tituloController.text;
+    String descricao = _descricaoController.text;
+
+    if( anotacaoSelecionada == null ){//salvar
+      Anotacao anotacao = Anotacao(titulo, descricao, DateTime.now().toString() );
+      int resultado = await _db.salvarAnotacao( anotacao );
+    }else{//atualizar
+      anotacaoSelecionada.titulo    = titulo;
+      anotacaoSelecionada.descricao = descricao;
+      anotacaoSelecionada.data      = DateTime.now().toString();
+      int resultado = await _db.atualizarAnotacao( anotacaoSelecionada );
+    }
+
+    _tituloController.clear();
+    _descricaoController.clear();
+
+    _recuperarAnotacoes();
+
+  }
+
+  _formatarData(String data){
+
+    initializeDateFormatting("pt_BR");
+
+    //Year -> y month-> M Day -> d
+    // Hour -> H minute -> m second -> s
+    //var formatador = DateFormat("d/MMMM/y H:m:s");
+    var formatador = DateFormat.yMd("pt_BR");
+
+    DateTime dataConvertida = DateTime.parse( data );
+    String dataFormatada = formatador.format( dataConvertida );
+
+    return dataFormatada;
+
+
+  }
+
+  _removerAnotacao(int id) async {
+
+    await _db.removerAnotacao( id );
+
+    _recuperarAnotacoes();
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarAnotacoes();
   }
 
   @override
@@ -68,7 +157,56 @@ class _HomeState extends State<Home> {
         title: Text("Minhas anotações"),
         backgroundColor: Colors.lightGreen,
       ),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView.builder(
+                  itemCount: _anotacoes.length,
+                  itemBuilder: (context, index){
+
+                    final anotacao = _anotacoes[index];
+
+                    return Card(
+                      child: ListTile(
+                        title: Text( anotacao.titulo ),
+                        subtitle: Text("${_formatarData(anotacao.data)} - ${anotacao.descricao}") ,
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: (){
+                                _exibirTelaCadastro(anotacao: anotacao);
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 16),
+                                child: Icon(
+                                  Icons.edit,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: (){
+                                _removerAnotacao( anotacao.id );
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 0),
+                                child: Icon(
+                                  Icons.remove_circle,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+
+                  }
+              )
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.green,
           foregroundColor: Colors.white,
--- flutter_app_minhas_anotacoes/lib/helper/AnotacaoHelper.dart
+++ flutter_app_minhas_anotacoes/lib/helper/AnotacaoHelper.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_app_minhas_anotacoes/model/Anotacao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+
 class AnotacaoHelper {
+
+  static final String nomeTabela = "anotacao";
 
   static final AnotacaoHelper _anotacaoHelper = AnotacaoHelper._internal();
   Database _db;
@@ -18,7 +22,8 @@ class AnotacaoHelper {
     if( _db != null ){
       return _db;
     }else{
-      return inicializarDB();
+      _db = await inicializarDB();
+      return _db;
     }
 
   }
@@ -32,7 +37,11 @@ class AnotacaoHelper {
 
     * */
 
-    String sql = "CREATE TABLE anotacao (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, descricao TEXT, data DATETIME)";
+    String sql = "CREATE TABLE $nomeTabela ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "titulo VARCHAR, "
+        "descricao TEXT, "
+        "data DATETIME)";
     await db.execute(sql);
 
   }
@@ -46,6 +55,47 @@ class AnotacaoHelper {
     return db;
 
   }
+
+  Future<int> salvarAnotacao(Anotacao anotacao) async {
+
+    var bancoDados = await db;
+    int resultado = await bancoDados.insert(nomeTabela, anotacao.toMap() );
+    return resultado;
+
+  }
+
+  recuperarAnotacoes() async {
+
+    var bancoDados = await db;
+    String sql = "SELECT * FROM $nomeTabela ORDER BY data DESC ";
+    List anotacoes = await bancoDados.rawQuery( sql );
+    return anotacoes;
+
+  }
+
+  Future<int> atualizarAnotacao(Anotacao anotacao) async {
+
+    var bancoDados = await db;
+    return await bancoDados.update(
+        nomeTabela,
+        anotacao.toMap(),
+        where: "id = ?",
+        whereArgs: [anotacao.id]
+    );
+
+  }
+
+  Future<int> removerAnotacao( int id ) async {
+
+    var bancoDados = await db;
+    return await bancoDados.delete(
+        nomeTabela,
+        where: "id = ?",
+        whereArgs: [id]
+    );
+
+  }
+
 
 }
 
