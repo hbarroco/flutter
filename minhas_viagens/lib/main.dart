04d661ac6f504ccf6439159c2bde92a2a8f70c19
Helio Barroco <hbarroco@ciandt.com>
2019-10-29 23:16:34 -0300

    Flutter Apps - Mapas

--- minhas_viagens/lib/main.dart
+++ minhas_viagens/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'Home.dart';
+import 'SplashScreen.dart';
 
-void main()
-{
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(
+  title: "Minhas viagens",
+  home: SplashScreen(),
+  debugShowCheckedModeBanner: false,
+));
