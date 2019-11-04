40845816a4d58297b95a80795565ff6254aed6be
Helio Barroco <heliobarroco@gmail.com>
2019-11-04 17:14:04 -0300

    Flutter Apps

---
+++ flutter_app_setando_icones_app/android/app/src/main/java/setandoiconesapp/apps/htb/flutter_app_setando_icones_app/MainActivity.java
@@ -0,0 +1,13 @@
+package setandoiconesapp.apps.htb.flutter_app_setando_icones_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
